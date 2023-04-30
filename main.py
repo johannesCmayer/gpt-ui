edbee2126c3b90620b0683dabf4766acf4988b54
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-04-30 22:58:02 +0100

    way to many changes at once

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
+from glob import glob
+import re
 import subprocess
-import openai
-import termcolor
 import signal
 from pathlib import Path
 import argparse
 import json
 import time
-import yaml
 import os
 import datetime
+import readline
+
+import tiktoken
+import yaml
+import openai
+import termcolor
 
 model = 'gpt-4'
+user = 'Johannes'
+max_tokens = 8192
 assistant_name = 'assistant'
 default_chat = [
         {"role": "system", "content": "You are a helpful assistant, that answers every question."},
@@ -29,27 +36,32 @@ parser = argparse.ArgumentParser(usage=
                                     "Enter 'help' as the user role to see all commands you can enter in the user role."))
 parser.add_argument('--chat-name', type=str, help='Name of the chat')
 parser.add_argument('--load-chat', type=str, help='Name of the chat to load')
+parser.add_argument('--load-last-chat', action='store_true', help='Name of the chat to load')
 parser.add_argument('--list-chats', action='store_true', help='List all chats')
 parser.add_argument('--list-all-chats', action='store_true', help='List all chats including hidden backup chats')
-parser.add_argument('--sync-speech', default=True, action='store_true', help='Sync speech with chat')
-parser.add_argument('--no-sync-speech', action='store_true', help='Sync speech with chat')
+parser.add_argument('--sync-speech', default=False, action='store_true', help='Sync speech with chat')
+parser.add_argument('--list-models', action='store_true', help='List all models')
+parser.add_argument('--speak', default=False, action='store_true', help='Speak the messages.')
 args = parser.parse_args()
 
-args.sync_speech = not args.no_sync_speech
+
+enc = tiktoken.encoding_for_model("gpt-4")
 
 def backup_chat(chat, name=None, prompt_name=None):
+    if chat == default_chat or len(chat) == 0:
+        return
     with chat_backup_file.open("w") as f:
-        json.dump(chat, f)
+        json.dump(chat, f, indent=4)
     if prompt_name:
         try:
-            name = input("Save name: ")
+            name = get_input("Save name: ")
             with (chat_dir / name).open("w") as f:
-                json.dump(chat, f)
+                json.dump(chat, f, indent=4)
         except EOFError as e:
             pass
     elif name:
         with (chat_dir / name).open("w") as f:
-            json.dump(chat, f)
+            json.dump(chat, f, indent=4)
 
 def color_role(s):
     if "system" in s:
@@ -71,6 +83,14 @@ def next_role(chat):
     else:
         return "user"
 
+def get_input(prompt):
+    try:
+        user_input = input(prompt)
+    except KeyboardInterrupt:
+        print("\nKeyboardInterrupt detected")
+        return None
+    return user_input
+
 ctrl_c = 0
 def signal_handler(sig, frame):
     global ctrl_c
@@ -79,6 +99,10 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 def main():
+    if args.list_models:
+        print(openai.Model.list())
+        exit(0)
+
     global ctrl_c
     chat_name = args.chat_name
 
@@ -91,6 +115,10 @@ def main():
     if args.load_chat:
         with (chat_dir / args.load_chat).open() as f:
             chat = json.load(f)
+    if args.load_last_chat:
+        chat_path = [x for x in sorted(chat_dir.iterdir()) if x.is_file() and x.name.startswith('.')][-1]
+        with chat_path.open() as f:
+            chat = json.load(f)
     else:
         chat = default_chat
 
@@ -101,7 +129,7 @@ def main():
         if active_role in ['user', 'system'] :
             ctrl_d = 0
             try:
-                user_input = input(color_role(f'{active_role}: '))
+                user_input = get_input(color_role(f'{active_role}: '))
             except EOFError as e:
                 print()
                 ctrl_d += 1
@@ -109,15 +137,18 @@ def main():
                 backup_chat(chat)
                 while not chat_name or (chat_dir / chat_name).exists() or chat_name == '':
                     try:
-                        chat_name = input('Save name: ')
+                        chat_name = get_input('Save name: ')
                     except EOFError as e:
                         ctrl_d += 1
                     if ctrl_d > 1 or chat_name == 'exit':
                         exit(0)
                     time.sleep(0.1)
                 with (chat_dir / chat_name).open("w") as f:
-                    json.dump(chat, f)
+                    json.dump(chat, f, indent=4)
                 exit(0)
+            elif user_input in ['pass']:
+                active_role = "assistant"
+                continue
             elif user_input in ['clear', 'cls']:
                 backup_chat(chat)
                 chat = []
@@ -137,7 +168,7 @@ def main():
                 for c in chat_dir.iterdir():
                     if not c.name.startswith('.'):
                         print(f"{c.name}")
-                chat_name = input('Name of chat to load: ')
+                chat_name = get_input('Name of chat to load: ')
                 if chat_name == 'exit':
                     continue
                 with chat_dir.joinpath(chat_name).open() as f:
@@ -148,33 +179,39 @@ def main():
                 continue 
             elif user_input in ['save', 'sv']:
                 while not chat_name or (chat_dir / chat_name).exists() or chat_name == '':
-                    chat_name = input('Name chat: ').strip()
+                    chat_name = get_input('Name chat: ').strip()
                     if chat_name == 'exit':
                         continue
                     time.sleep(0.1)
                 with (chat_dir / chat_name).open("w") as f:
-                    json.dump(chat, f)
+                    json.dump(chat, f, indent=4)
                 continue
             elif user_input in ['vi', 'vim', 'nvim']:
                 backup_chat(chat)
                 with (chat_dir / 'temp').open("w") as f:
                     for m in chat:
-                        f.write(f"{m['role']}:\n{m['content']}\n\n")
+                        prefix = json.dumps({k: v for k, v in m.items() if k != 'content'})
+                        f.write(f"{prefix}\n{m['content']}\n\n")
                 os.system(f"{user_input} {chat_dir / 'temp'}")
                 with (chat_dir / 'temp').open() as f:
                     chat = []
                     role = None
                     text = ""
                     for line in f:
-                        if line.replace(':', '').strip() in ['system', 'user', 'assistant']:
-                            if role != None:
-                                chat.append({"role": role, "content": text.strip()})
-                                backup_chat(chat)
-                                text = ""
-                            role = line.replace(':', '').strip()
+                        r = None
+                        try:
+                            r = json.loads(line)
+                        except:
+                            pass
+                        if r:
+                            if role:
+                                append_to_chat(chat, role, text.strip())
+                            backup_chat(chat)
+                            text = ""
+                            role = r['role']
                         else:
                             text += line
-                    chat.append({"role": role, "content": text.strip()})
+                    append_to_chat(chat, role, text.strip())
                     backup_chat(chat)
                 (chat_dir / 'temp').unlink()
                 print('\n\n')
@@ -205,21 +242,24 @@ def main():
                 help/h: show this message
                 ''')
                 continue
-            chat.append({"role": active_role, "content": user_input})
+            append_to_chat(chat, active_role, user_input)
             backup_chat(chat)
             active_role = next_role(chat)
         elif active_role == 'assistant':
-            try:
-                response = openai.ChatCompletion.create(
-                    model=model,
-                    messages=chat,
-                    stream = True,
-                )
-            except Exception as e:
-                backup_chat(chat)
-                exit(1)
+            while True:
+                chat, num_tokens = trim_chat(chat)
+                try:
+                    response = openai.ChatCompletion.create(
+                        model=model,
+                        messages=[{k: v for k, v in y.items() if k in ['role', 'content']} for y in chat],
+                        stream = True,
+                    )
+                    break
+                except Exception as e:
+                    backup_chat(chat)
+                    raise {e}
             complete_response = []
-            print(color_role(f'{assistant_name}: '), end='', flush=True)
+            print(color_role(f'{int(num_tokens/max_tokens*100)}% {model}: '), end='', flush=True)
             read_buffer = ''
             subpc = None
             for chunk in response:
@@ -235,35 +275,50 @@ def main():
                 except AttributeError as e:
                     pass
                 except Exception as e:
-                    print("An error occured.")
+                    print("\nAn error occured.")
                     backup_chat(chat, prompt_name=True)
                     raise e
+                    
                 if subpc is None or subpc.poll() is not None:
                     for i,c in enumerate(read_buffer):
-                        if c in ['.', '?', '!']:
+                        if c in ['. ', '? ', '! ', '\n']:
                             reading_buffer = read_buffer[:i+1]
                             read_buffer = read_buffer[i+1:]
-                            subpc = subprocess.Popen(['say', reading_buffer])
-                            if args.sync_speech:
-                                print(reading_buffer, end='', flush=True)
-                            break
+                            if args.speak:
+                                subpc = subprocess.Popen(['say', reading_buffer])
+                                if args.sync_speech:
+                                    print(reading_buffer, end='', flush=True)
+                                break
             while read_buffer != '':
                 if subpc is None or subpc.poll() is not None:
                     for i,c in enumerate(read_buffer):
                         if c in ['.', '?', '!']:
                             reading_buffer = read_buffer[:i+1]
                             read_buffer = read_buffer[i+1:]
-                            subpc = subprocess.Popen(['say', reading_buffer])
-                            if args.sync_speech:
-                                print(reading_buffer, end='', flush=True)
+                            if args.speak:
+                                subpc = subprocess.Popen(['say', reading_buffer])
+                                if args.sync_speech:
+                                    print(reading_buffer, end='', flush=True)
                             break
                     else:
                         break
             print()
             complete_response = ''.join(complete_response)
-            chat.append({"role": "assistant", "content": complete_response})
-            backup_chat(chat)
+            append_to_chat(chat, 'assistant', complete_response)
             active_role = next_role(chat)
+
+def trim_chat(chat):
+    num_tokens = 0
+    for i, e in enumerate(reversed(chat)):
+        num_tokens += len(enc.encode(e['content']))
+        if num_tokens > max_tokens:
+            return list(reversed(reversed(chat)[:i])), num_tokens
+    return chat, num_tokens
+
+def append_to_chat(chat, role, content, l_date=None, l_model=None):
+    date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    chat.append({'date': l_date if l_date else date, "model": l_model if l_model else model, "role": role, "content": content})
+    backup_chat(chat)
 
 if __name__ == "__main__":
     main()
